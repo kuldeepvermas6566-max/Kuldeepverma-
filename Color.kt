3b5e985e2b941daa7be9f81ebdabafbbcda5a95f
kuldeepvermas6566-max <kuldeepvermas6566@gmail.com>
2026-07-31 21:37:20 +0530

    app/src/main/java/com/kuldeep/momento/ui/theme/Color.kt

---
+++ Color.kt
@@ -0,0 +1,7 @@
+package com.kuldeep.momento.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MomentoPrimary = Color(0xFF6750A4)
+val MomentoSecondary = Color(0xFF625B71)
+val MomentoTertiary = Color(0xFF7D5260)
