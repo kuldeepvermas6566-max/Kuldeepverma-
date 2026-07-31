2c82aac31ee22a005cf4d84a0c59b06aca4241ca
kuldeepvermas6566-max <kuldeepvermas6566@gmail.com>
2026-07-31 21:28:47 +0530

    app/src/main/java/com/kuldeep/momento/core/DispatcherProvider.kt

---
+++ DispatcherProvider.kt
@@ -0,0 +1,16 @@
+package com.kuldeep.momento.core
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+}
+
+object DefaultDispatcherProvider : DispatcherProvider {
+    override val main = Dispatchers.Main
+    override val io = Dispatchers.IO
+    override val default = Dispatchers.Default
+}
